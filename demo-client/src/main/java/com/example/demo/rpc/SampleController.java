aa4a3b2edf375728c268226379883cdde884c784
eiltpeh <ilter.pehlivan@ericsson.com>
2020-06-08 16:14:00 -0400

    fix token and print metrics

--- demo-client/src/main/java/com/example/demo/rpc/SampleController.java
+++ demo-client/src/main/java/com/example/demo/rpc/SampleController.java
@@ -1,6 +1,11 @@
 package com.example.demo.rpc;
 
 import com.example.demo.svc.SimpleRequest;
+import io.micrometer.core.instrument.Meter;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.search.Search;
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
+import java.util.Collection;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +18,7 @@ import reactor.core.publisher.Mono;
 public class SampleController {
 
   @Autowired ServiceConfig serviceConfig;
+  @Autowired MeterRegistry meterRegistry;
 
   @RequestMapping("/hello")
   public Mono<String> sayHello() {
@@ -23,6 +29,32 @@ public class SampleController {
         .doOnNext(
             simpleResponse ->
                 log.info("response from service {}", simpleResponse.getResponseMessage()))
-        .map(simpleResponse -> simpleResponse.getResponseMessage());
+        .map(simpleResponse -> simpleResponse.getResponseMessage())
+        .doFinally(
+            signalType -> {
+              log.info("doFinally:signal {} .. printing metrics",signalType);
+//              printClientMetrics("request.RequestReply");
+              printAllMetrics();
+            });
+  }
+
+  private void printClientMetrics(String name) {
+    Search search = meterRegistry.find(name);
+    Collection<Meter> meters = search.meters();
+    if (meters != null) {
+      meters.forEach(
+          meter -> {
+            log.info("client meter id {} result {}", meter.getId(), meter.measure());
+          });
+    } else {
+      log.info("Client meter is null {}", name);
+    }
+  }
+
+  private void printAllMetrics(){
+    meterRegistry.forEachMeter(
+        meter -> {
+          log.info("client meter id {} result {}", meter.getId(), meter.measure());
+        });
   }
 }
